8d394564a261eaff19054a2f03f37c72e4129767
Luiz Felipe Escandiuzzi <mrescandiuzzi@gmail.com>
2018-09-04 17:25:43 -0300

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rigidbody;
+
+    [SerializeField]
+    float playerSpeed;
+
+    bool moving = false;
+
+	// Use this for initialization
+	void Start () {
+
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Movement();
+    }
+
+    public void Movement()
+    {
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.up * playerSpeed * Time.deltaTime, Space.World);
+            moving = true;
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.down * playerSpeed * Time.deltaTime, Space.World);
+            moving = true;
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * playerSpeed * Time.deltaTime, Space.World);
+            moving = true;
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * playerSpeed * Time.deltaTime, Space.World);
+            moving = true;
+        }
+
+        if (Input.GetKey(KeyCode.W) != true && Input.GetKey(KeyCode.S) != true && Input.GetKey(KeyCode.A) != true && Input.GetKey(KeyCode.D) != true && Input.GetKey(KeyCode.UpArrow) != true && Input.GetKey(KeyCode.DownArrow) != true && Input.GetKey(KeyCode.LeftArrow) != true && Input.GetKey(KeyCode.RightArrow) != true)
+        {
+            moving = false;
+        }
+
+    }
+
+}
